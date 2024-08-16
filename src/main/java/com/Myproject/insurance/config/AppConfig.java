f49090b5e15e49b5fe40e5eea5a34363a7503094
heerinheerin <keejoo95@naver.com>
2024-08-16 19:07:36 +0900

    8-16

--- src/main/java/com/Myproject/insurance/config/AppConfig.java
+++ src/main/java/com/Myproject/insurance/config/AppConfig.java
@@ -8,5 +8,16 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 
+    String apiKey = "6670820564805133";
+    String secretKey = "Wghm8njzidJI4XhfBuxRDb3cXVlVXqJwz7xMd7VXpJvDlM2pLgdjNJzvKGQU60T2QhZC2msAWZloOqBc";
 
+    @Bean
+    public IamportClient iamportClient() {
+        return new IamportClient(apiKey, secretKey);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
